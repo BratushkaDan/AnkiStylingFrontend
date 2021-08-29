1ab042f7dd1d0c775f6c115b713e1a8f454b0ae3
BratushkaDan <saderdan9@gmail.com>
2021-08-29 22:36:32 +0300

    Promise API is removed all over from highlighting code. JS code input error fixed

--- src/modules/anki/utils/index.js
+++ src/modules/anki/utils/index.js
@@ -1,13 +1,9 @@
 import Prism from 'prismjs';
+import 'prismjs/components/prism-regex.min.js';
 
-export async function processRegex(code) {
-  await import('prismjs/components/prism-regex.min.js');
-  function regex(code) {
-    return Prism.highlight(code, Prism.languages.regex, 'regex');
-  }
-
+export function processRegex(code) {
   let r = /<span class="token regex-source language-regex">(.+?)?<\/span>/gm;
   let m = code.match(r);
-  if (code.match(r)) for (let match of m) code = code.replace(r, regex(match.split(r)[1]));
+  if (code.match(r)) for (let match of m) code = code.replace(r, Prism.highlight(match.split(r)[1], Prism.languages.regex, 'regex'));
   return code;
 }
