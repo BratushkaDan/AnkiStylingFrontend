e0e4f57cad2bfc5e7a2ab5b17c05fef0113e9718
BratushkaDan <saderdan9@gmail.com>
2021-09-11 11:14:31 +0300

    removed <h1> tags from the interface

--- src/App.js
+++ src/App.js
@@ -12,12 +12,10 @@ function App() {
   return (
     <div className="App">
       <LeftColumn>
-        <h1>Input</h1>
         <Menu/>
         <FormField/>
       </LeftColumn>
       <RightColumn>
-        <h1>Output</h1>
         <RendererField/>
       </RightColumn>
     </div>
